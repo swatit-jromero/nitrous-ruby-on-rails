96a6fed18979993d9a82f5e0135b03b715afa92b
Jorge Mario Romero Arroyo <jromero@swat-it.co>
2014-06-05 19:37:31 +0000

    finish practice

--- workshop/rails/practice/user-demo/app/controllers/users_controller.rb
+++ workshop/rails/practice/user-demo/app/controllers/users_controller.rb
@@ -2,50 +2,70 @@ class UsersController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
-
+    
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    
     def index
-        
         @userList = User.all
-        
     end
     
     def show
-        
-        @user = User.find(params[:id])
-        
     end
     
     def new
-        
-        @user = User.new
-        
+        @user = User.new 
     end
     
-    def user_params
-        
-        params.require(:user).permit(:name, :email)
-        
+    def edit
+        if session[:id]        
+        end
     end
-    
+
     def create
         
         @user = User.new(user_params)
         
         respond_to do |format|
-           
             if @user.save
-                
-                format.html { redirect_to @user, notice: 'New User Created' }
+                format.html { redirect_to @user, notice: 'New User Created!' }
                 format.json { render :show, status: :created, location: @user }
-                
             else
-                
                 format.html { render :new }
                 format.json { render json: @user.errors, status: :unprocessable_entity }                
             end
-            
         end
+    end
+    
+    def update
+        respond_to do |format|
+            if @user.update(user_params)
+                format.html { redirect_to @user, notice: 'User Updated!' }
+                format.json { render :show, status: :created, location: @user }
+            else
+                format.html { render :new }
+                format.json { render json: @user.errors, status: :unprocessable_entity }                
+            end
+        end
+    end
+    
+    def destroy
         
+        @user.destroy
+
+        respond_to do |format|
+            format.html { redirect_to users_url, notice: 'User Deleted!' }
+            format.json { head :ok }
+        end
     end    
+        
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end    
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
     
 end
