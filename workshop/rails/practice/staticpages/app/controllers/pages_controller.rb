30427e3fb2767cd5390602228665420498c9de7c
Jorge Mario Romero Arroyo <jromero@swat-it.co>
2014-06-06 18:34:57 -0500

    Commit section five

--- workshop/rails/practice/staticpages/app/controllers/pages_controller.rb
+++ workshop/rails/practice/staticpages/app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+    
   def home
   end
 
   def about
   end
+
+  def help
+  end
+
 end
--- workshop/rails/practice/staticpages/config/routes.rb
+++ workshop/rails/practice/staticpages/config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'pages/home'
-
   get 'pages/about'
+  get "pages/help"
+  get "pages/contact"
+
+  get '/pages' => 'pages#home', as: 'pages_index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
